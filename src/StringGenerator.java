c8d0ffdcd99e1a6a7177eb038e4d69947913636f
MossaabDev <mossbachagha@gmail.com>
2023-08-20 17:49:32 +0100

    string generator optim

--- src/StringGenerator.java
+++ src/StringGenerator.java
@@ -2,17 +2,18 @@ import java.util.List;
 
 public class StringGenerator {
     //all allowed charcters
-    private String allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private char[] allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+    StringBuilder result = new StringBuilder();
     public StringGenerator() {
     }
     public String generateString(int num) {
-        int a;
-        String result = "";
+        result.setLength(0);
         while (num >= 0) {
-            result = result + allowedChars.charAt(num % allowedChars.length());
-            num = (num / allowedChars.length()) - 1;
+            result.append(allowedChars[num % allowedChars.length]);
+            num = (num / allowedChars.length) - 1;
+
         }
-        return result;
+        return result.toString();
     }
 
 
